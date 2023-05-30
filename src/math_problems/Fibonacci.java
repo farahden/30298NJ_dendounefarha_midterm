69725751cfcbf765674bfd780a8484887cd0a1bf
farahden <dendounefarha@gmail.com>
2023-05-29 23:53:51 -0400

    fibonacci done

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -7,8 +7,15 @@ public class Fibonacci {
      * Write a method that will print or return at least 40 Fibonacci number
      * e.g. - 0,1,1,2,3,5,8,13
      **/
+private static  long fibonacci(int n){
+    if(n<=1) {
+        return n;
+    }return (fibonacci(n-1)+fibonacci(n-2));
+}
+
 
     public static void main(String[] args) {
-
+int n=40;
+        System.out.println(fibonacci(n));
     }
 }
