8bb81d6a4ee7a30e6b0d7badc73136a833223124
farahden <dendounefarha@gmail.com>
2023-05-30 00:51:33 -0400

    prime number is done

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -1,5 +1,7 @@
 package math_problems;
 
+import java.util.ArrayList;
+import java.util.List;
 public class PrimeNumber {
 
     /** INSTRUCTIONS
@@ -11,6 +13,25 @@ public class PrimeNumber {
 
 
     public static void main(String[] args) {
+        List<Integer>primeNumber=new ArrayList<>();
+        for (int numToCheck=2;numToCheck<=1000000;numToCheck++){
+            boolean isPrime = true;
+            for(int factor=2;factor<=numToCheck;factor++){
+                if(numToCheck%factor==0){
+                   isPrime=false;{
+                    break;
+                }
+            }
+            if (isPrime){
+            primeNumber.add(numToCheck) ;
+            }
+            }
+            System.out.println("prime numbers from 1 to 1 000 000 are:\n");
+        for(int number:primeNumber){
+            System.out.println(number);
+        }
+        }
+
 
     }
 
