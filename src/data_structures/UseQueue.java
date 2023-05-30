e1824836dd4b62de687475931796aea8d97b2b97
farahden <dendounefarha@gmail.com>
2023-05-30 01:29:18 -0400

    queue is done

--- src/data_structures/UseQueue.java
+++ src/data_structures/UseQueue.java
@@ -1,5 +1,9 @@
 package data_structures;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
     /** INSTRUCTIONS
@@ -12,6 +16,50 @@ public class UseQueue {
 
 
     public static void main(String[] args) {
+        //queue using peek poll and add
+        try{
+        Queue queue=new LinkedList<>();
+        queue.add("Dior");
+        queue.add("Chanel");
+        queue.add("Gucci");
+        queue.add("Lancom");
+        queue.add("Versace");
+        System.out.println(queue);
+
+        queue.peek();
+        System.out.println(queue);
+
+        queue.poll();
+        System.out.println(queue);
+
+        queue.remove();
+        System.out.println(queue);
+
+        //using forloop:
+        Iterator value=queue.iterator();
+        while(value.hasNext()){
+            System.out.println(queue);
+            //to add value:
+            System.out.println(queue.add("Yve Saint Laurant"));
+            //to execute a value without retrieving
+            System.out.println(queue.peek());
+            //to retrieve a value
+            System.out.println(queue.poll());
+            for (Iterator i = queue.iterator();i.hasNext();) {
+                System.out.print(i.next() + ", ");
+            }
+
+        }
+        }catch (NullPointerException e){
+            System.out.println("queue is empty");
+        }
+
+
+
+
+
+
+
 
     }
 
