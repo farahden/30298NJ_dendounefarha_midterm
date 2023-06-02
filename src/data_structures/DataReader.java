6a63bbef57b9b0a1a435eeb3b9b8bfdfe0b110ee
farahden <dendounefarha@gmail.com>
2023-06-02 00:19:25 -0400

    data reader almost done

--- src/data_structures/DataReader.java
+++ src/data_structures/DataReader.java
@@ -1,5 +1,15 @@
 package data_structures;
 
+import com.mysql.cj.jdbc.integration.c3p0.MysqlConnectionTester;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class DataReader {
 
     /** INSTRUCTIONS
@@ -20,9 +30,32 @@ public class DataReader {
 
     public static void main(String[] args) {
         String textFilePath = System.getProperty("user.dir") + "/src/data_structures/data/self-driving-car.txt";
+        MysqlConnectionTester connection = new MysqlConnectionTester();
+        BufferedReader reader = null;
+        ArrayList<Iterator> convertTextToList = new ArrayList<>();
+        try {
+
+           // connection.activeCheckConnection(connection1);
+            //reader = new BufferedReader;
+            String textLine = reader.toString();
+            System.out.println("***************");
+            while (textLine != null) {
 
 
+                textLine = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            System.out.println("IOException occured");
+        } catch (Exception e) {
+// System.out.println("an Exception happened");
+            e.fillInStackTrace();
+            e.getMessage();
+            //System.out.println("$$");
+// words = textLine.split(" ", -2);//split again the line to get seperate words
+            //tomysql.insertDataFromStringdToSqlTable(textLine +" " +br.readLine(), "Data_Reader_Mentoring", "Self_Driving");
 
+        }
     }
-
 }
