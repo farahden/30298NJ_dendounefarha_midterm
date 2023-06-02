3bb84c50c9554bdd8061551b50705ad6d9d5c279
farahden <dendounefarha@gmail.com>
2023-06-02 00:41:26 -0400

    Anagram is done

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,5 +1,8 @@
 package string_problems;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Anagram {
 
     /** INSTRUCTIONS
@@ -14,5 +17,36 @@ public class Anagram {
      */
 
     // Implement Here
+    public static void main(String[] args) {
+        String S1 ;
+        String S2;
+        System.out.println("please enter the first word");
+        Scanner sc1=new Scanner(System.in);
+        S1= sc1.next();
+        System.out.println("please enter 2nd word");
+        Scanner sc2=new Scanner(System.in);
+        S2= sc2.next();
+        if(anagramCheck(S1,S2)){
+            System.out.println("are: Anagrams");
+        }else{
+            System.out.println("are not Anagrams");
+        }
+    }
+    static boolean anagramCheck(String S1, String S2) {
+        if (S1.length() != S2.length()) {
+            return false;
+        }
+        char string1ToArray[]=S1.toCharArray();
+        char string2ToArray[]=S2.toCharArray();
+
+        Arrays.sort(string1ToArray);
+        Arrays.sort(string2ToArray);
+        for(int i=0;i<S1.length();i++){
+            if(string1ToArray[i]!=string2ToArray[i]){
+                return false;
+            }
+        }return true;
+
+    }
 
 }
