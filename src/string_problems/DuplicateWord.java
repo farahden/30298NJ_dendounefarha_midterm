133dd3ad71bbb4567fb5cb92928f0da99c910ab1
farahden <dendounefarha@gmail.com>
2023-06-02 01:19:10 -0400

    part of duplicateWord is done

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,5 +1,7 @@
 package string_problems;
 
+import java.util.*;
+
 public class DuplicateWord {
 
     /** INSTRUCTIONS
@@ -11,7 +13,48 @@ public class DuplicateWord {
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
         // Implement here
+       countandWords(String s);
+        FindDuplicateWords(String string);
 
-    }
+        }
+    public static void FindDuplicateWords(String string ){
+        int count;
+        string=string.toLowerCase();
+        String[] words=string.replace(",","").split(" ");
+        System.out.println("Duplicated words are: ");
+        int wordLengthCounter=0;
+        int duplicateWordCount=0;
+        for(int i=0;i<words.length;i++) {
+            count = 1;
+            int j;
+            for (j = i + 1; i < words.length; j++) {
+                count++;
+                words[j] = "0";
+            }
 
-}
+
+            if (count >= 1 && words[j] != "0") {
+                System.out.println("word " + words[i] + ",occurrences: ");
+                wordLengthCounter += words[i].length();
+                duplicateWordCount++;
+            }
+        }
+           /* public static void countandWords (String string){
+                List<String> output = new ArrayList<>();
+                List<Integer> count = new ArrayList<>();
+                findWords(s, output, count);
+                System.out.println(output);
+                System.out.println(count);
+            }
+            public static void findWords (String s, List < String > output, List < Integer > count){
+                String[] words = s.split(" ", -2);
+                Map<String, Integer> map = new LinkedHashMap<>();
+                Arrays.stream(words).forEach(e -> map.put(e, map.getOrDefault(e, 0) + 1));
+                map.forEach((k, v) -> {
+                    output.add(k);
+                    count.add(v);
+                });
+
+            }
+
+        }
