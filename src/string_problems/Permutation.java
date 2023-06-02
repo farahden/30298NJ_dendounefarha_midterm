fe0edf6a799455f5472828e26e58f4cbd6bd4d86
farahden <dendounefarha@gmail.com>
2023-06-02 01:27:52 -0400

    permutation verified and done

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -10,6 +10,31 @@ public class Permutation {
 
     public static void main(String[] args) {
 
-    }
+
     // Implement Here
+    String s="ABC";
+    int n=s.length();
+    Permutation permutation=new Permutation();
+permutation.calculate(s,0,n-1);
+}
+
+    private void calculate(String str, int left, int right) {
+        if(left==right){
+            System.out.print(" "+str);
+        }
+        else{
+            for(int i=left;i<=right;i++){
+                String swapped=this.swap(str,left,i);
+                this.calculate(swapped,left+1,right);
+            }
+        }
+    }
+    private String swap(String str,int i,int j){
+        char temp;
+        char[] charArray=str.toCharArray();
+        temp= charArray[i];
+        charArray[i]=charArray[j];
+        charArray[j]=temp;
+        return String.valueOf(charArray);
+    }
 }
